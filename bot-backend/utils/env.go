90970047166d99de28ca9b202f13b01dd362fe3d
EdgeJay <hjwu85@gmail.com>
2023-01-15 21:41:24 +0800

    add functions to retrieve env vars

---
+++ bot-backend/utils/env.go
@@ -0,0 +1,19 @@
+package utils
+
+import "os"
+
+func GetAppEnv() string {
+	return os.Getenv("app_env")
+}
+
+func IsProductionEnv() bool {
+	return GetAppEnv() == "prod"
+}
+
+func GetTelegramBotToken() string {
+	return os.Getenv("bot_token")
+}
+
+func GetLambdaInvokeUrl() string {
+	return os.Getenv("lambda_invoke_url")
+}
